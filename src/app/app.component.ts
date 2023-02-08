1134b72cd58a646bba9c5b2d4000fa696aeb3815
Muhammad Owais <rahneverd@gmail.com>
2023-02-08 15:53:10 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-age-calculator';
+  dobForm;
+  timeDiff;
+  constructor(private _fb: FormBuilder) {
+    this.dobForm = this._fb.group({
+      dob: ['', [Validators.required]],
+    });
+  }
+  calculate() {
+    console.log('called');
+    let todaysDate = new Date();
+    let dob = new Date(this.dobForm.controls['dob'].value);
+    let days = todaysDate.getDate() - dob.getDate();
+    let months = todaysDate.getMonth() - dob.getMonth();
+    let years = todaysDate.getFullYear() - dob.getFullYear();
+    if (days < 0) {
+      months = months - 1;
+      days = 30 + days;
+    }
+    if (months < 0) {
+      years = years - 1;
+      months = 11;
+    }
+    this.timeDiff =
+      years + ' years, ' + months + ' months and ' + days + ' days.';
+    console.log(this.timeDiff);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
