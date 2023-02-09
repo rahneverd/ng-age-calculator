229a5a67ffb055764cc59f8846914aa09ab7ba48
Muhammad Owais <rahneverd@gmail.com>
2023-02-09 17:44:12 +0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -16,20 +16,51 @@ export class AppComponent {
     });
   }
   calculate() {
-    console.log('called');
+    //
     let todaysDate = new Date();
     let dob = new Date(this.dobForm.controls['dob'].value);
-    let days = todaysDate.getDate() - dob.getDate();
-    let months = todaysDate.getMonth() - dob.getMonth();
-    let years = todaysDate.getFullYear() - dob.getFullYear();
-    if (days < 0) {
-      months = months - 1;
-      days = 30 + days;
+    let FromYear = dob.getFullYear();
+    let FromMonth = dob.getMonth() + 1;
+    let FromDay = dob.getDate();
+
+    //current date
+    let ToYear = todaysDate.getFullYear();
+    let ToMonth = todaysDate.getMonth() + 1;
+    let ToDay = todaysDate.getDate();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let years, months, days;
+
+    if (FromDay > ToDay) {
+      ToDay = ToDay + month[ToMonth - 1];
+      ToMonth = ToMonth - 1;
     }
-    if (months < 0) {
-      years = years - 1;
-      months = 11;
+    if (FromMonth > ToMonth) {
+      ToMonth = ToMonth + 12;
+      ToYear = ToYear - 1;
     }
+    days = ToDay - FromDay;
+    months = ToMonth - FromMonth;
+    years = ToYear - FromYear;
+
+    // days = 30 + ToDay - FromDay;
+    // months = 12 + ToMonth - FromMonth;
+    // years = ToYear - 1 - FromYear;
+
+    // if (days >= 30) {
+    //   days = 0 + (days % 30);
+    //   months = months + 1;
+    // } else {
+    //   days = days;
+    //   months = months;
+    // }
+
+    // if (months >= 12) {
+    //   months = months % 12;
+    //   years = years + 1;
+    // } else {
+    //   months = months;
+    //   years = years;
+    // }
     this.timeDiff =
       years + ' years, ' + months + ' months and ' + days + ' days.';
     console.log(this.timeDiff);
